e8413159c8162de2881b28629ea4b27e76fbfb7b
Wai138 <92008242+Wai138@users.noreply.github.com>
2021-10-17 23:27:26 +1100

    night

--- timer.js
+++ timer.js
@@ -31,9 +31,11 @@ var time_elapsed_h;
 var time_elapsed_m;
 var time_elapsed_s;
 
+var result_h, result_m, result_s;
+
 init ();
 loadExerciseContainer();
-hightlightExercise();
+setupExerciseContainer();
 window.onscroll=stop();
 PopulateVoices();
 if(speechSynthesis !== undefined){
@@ -74,6 +76,7 @@ function init () {
 
 btnGO.addEventListener('click', () => {
     init ();
+    setupExerciseContainer();
     startExercise();
 });
 
@@ -138,54 +141,135 @@ function PopulateVoices(){
     voiceList.selectedIndex = selectedIndex;
 }
 
+function setupExerciseContainer () {
+    removeHighlight ();
+    scrollToNextExercise ();
+    hightlightExercise ();
+}
+
 function scrollToNextExercise () {
-    let exerId = 'exer' + exer_idx;
+    let exerId = 'E' + ("0" + set_idx).substr(-2) + ("0" + exer_idx).substr(-2);
     let elm = document.getElementById(exerId);
     elm.scrollIntoView();
 }
 function removeHighlight () {
-    let idx = exer_idx - 1;
-    let exerId = 'exer' + idx;
-    let elm = document.getElementById(exerId);
-    elm.style.backgroundColor = "white";
+    let exerId, elm;
+    for (let i=0;i<Sets.length;i++) {
+        for (let j=0;j<Sets[i].Exercises.length;j++){
+            exerId = 'E' + ("0" + i).substr(-2) + ("0" + j).substr(-2);
+            elm = document.getElementById(exerId);
+            elm.style.backgroundColor = "white";
+        }
+    }
 }
 
 function hightlightExercise() {
-    let exerId = 'exer' + exer_idx;
+    let exerId = 'E' + ("0" + set_idx).substr(-2) + ("0" + exer_idx).substr(-2);
     let elm = document.getElementById(exerId);
     elm.style.backgroundColor = "rgb(209, 153, 255)";
 }
 
 function loadExerciseContainer () {
-    document.getElementById('exerciseContainer').innerHTML = '';
+    let remain_time_h = rout_total_time_h;
+    let remain_time_m = rout_total_time_m;
+    let remain_time_s = rout_total_time_s;
+    let elapse_time_h, elapse_time_m, elapse_time_s; 
+    let ss, xx;
+    let Rhhmmss, Ehhmmss;
+    let temp;
+    document.getElementById('exerciseContainer').innerHTML ='';
 
-    for (i=0;i<Sets[set_idx].Exercises.length;i++){
-        let currExer  = Sets[set_idx].Exercises[i];
-        let duration  = ("0" + currExer.timer_exer_h).substr(-2) + ":" +
-                        ("0" + currExer.timer_exer_m).substr(-2) + ":" +
-                        ("0" + currExer.timer_exer_s).substr(-2);
-        let rest_time = ("0" + currExer.timer_rest_h).substr(-2) + ":" +
-                        ("0" + currExer.timer_rest_m).substr(-2) + ":" +
-                        ("0" + currExer.timer_rest_s).substr(-2);
+    for (let i=0;i<Sets.length;i++) {
+        ss = ("0" + i).substr(-2);
+//        xx = ("0" + Sets[i].set_iter).substr(-2);
 
-        let butn = `<table id="exer` + i + `" class="exerciseItem" style="width:100vw;">
-        <tr style="white-space: nowrap;">
-            <td style="height: .3em;text-align:left;font-weight: bold; font-size: 1.3em;width: 100vw";>` + currExer.exer_name + `</td>
-        </tr>
-        <tr style="height: .4em;line-height: 0.4;">
-            <td style="text-align:left;width:33.3333%;">Duration: 
-                <span id="duration">` + duration + `</span></td>
-            <td style="text-align:center;width:33.3333%;"></td>
-            <td style="text-align:left;width:33.3333%;">Rest: 
-                <span id="rest_time">` + rest_time + `</span></td>
-        </tr>
-        <tr style="height: .5em;line-height: 0.4;">
-            <td style="text-align:left;">Execute Count: <span>` 
-                + Sets[set_idx].Exercises[i].exer_iter + `</span></td>
-        </tr>
-    </table>`;
-        document.getElementById('exerciseContainer').innerHTML += butn;
+        temp = `   
+        <table style="width:100vw;background-color: black;height: 5vh;" id="S` + ss + `">
+        <tbody>
+        <tr class="oneThird set_header">
+        <td style="text-align:left;";>
+        <span style="color: white;">` + Sets[i].set_name + `</span></td>
+        <td style="text-align:center;"></td>
+        <td style="text-align:right;">
+            <span>Execute Count:</span>
+            <span>` + Sets[i].set_iter + `</span>
+        </td></tr></tbody></table>`;
+
+        console.log ('SSSSSSSSSSSSSSSSSSSSSSeeeetttt: ' + temp)
+        document.getElementById('exerciseContainer').innerHTML += temp;
+
+        for (let j=0;j<Sets[i].Exercises.length;j++){
+            let ee        = ("0" + j).substr(-2);
+            let currExer  = Sets[i].Exercises[j];
+            let duration  = ("0" + currExer.timer_exer_h).substr(-2) + ":" +
+                            ("0" + currExer.timer_exer_m).substr(-2) + ":" +
+                            ("0" + currExer.timer_exer_s).substr(-2);
+            let rest_time = ("0" + currExer.timer_rest_h).substr(-2) + ":" +
+                            ("0" + currExer.timer_rest_m).substr(-2) + ":" +
+                            ("0" + currExer.timer_rest_s).substr(-2);
+            
+            Rhhmmss = ("0" + remain_time_h).substr(-2) +
+            ("0" + remain_time_m).substr(-2) +
+            ("0" + remain_time_s).substr(-2);
+
+            calculateTimeDiff(rout_total_time_h, rout_total_time_m, rout_total_time_s,
+                remain_time_h, remain_time_m, remain_time_s);
+        
+            Ehhmmss = ("0" + result_h).substr(-2) +
+                ("0" + result_m).substr(-2) +
+                ("0" + result_s).substr(-2);
+
+            calculateTimeDiff(remain_time_h, remain_time_m, remain_time_s,
+                                Sets[i].Exercises[j].timer_exer_h, 
+                                Sets[i].Exercises[j].timer_exer_m, 
+                                Sets[i].Exercises[j].timer_exer_s);
+
+            remain_time_h = result_h;                    
+            remain_time_m = result_m;                    
+            remain_time_s = result_s;                    
+                        
+            let butn = `<table id="E` + ss + ee + 
+            `" class="exerciseItem" style="width:100vw;"><tbody>
+            <tr style="white-space: nowrap;">
+                <td style="height: .3em;text-align:left;font-weight: bold; font-size: 1.3em;width: 100vw";>` 
+                + currExer.exer_name + `</td>
+            </tr>
+            <tr style="height: .4em;line-height: 0.4;">
+                <td style="text-align:left;width:33.3333%;">Duration: 
+                    <span id="duration">` + duration + `</span></td>
+                <td style="text-align:center;width:33.3333%;"></td>
+                <td style="text-align:left;width:33.3333%;">Rest: 
+                    <span id="rest_time">` + rest_time + `</span></td>
+            </tr>
+            <tr style="height: .5em;line-height: 0.4;">
+                <td style="text-align:left;">Execute Count: <span>` 
+                    + Sets[i].Exercises[j].exer_iter + `</span></td>
+            </tr>
+            <span id="E` + ss + ee + `Remain" style="display: none;">
+            ` + Rhhmmss + `</span>
+            <span id="E` + ss + ee + `Elapse" style="display: none;">
+            ` + Ehhmmss + `</span>
+        </tbody></table>`;
+
+console.log ('EEEEEEExxxxxxxxxercissssssssssssssBUTN: ' + butn)
+            document.getElementById('exerciseContainer').innerHTML += butn;
+            console.log ('dddddddddddddddddddddddddddddddd: ' + document.getElementById('exerciseContainer').innerHTML)
+        }
     }
+}
+
+function calculateTimeDiff (h1, m1, s1, h2, m2, s2) {
+    if (s1 < s2) {
+        result_s = 10 + s2 - s1;
+        m1--;
+    } else result_s = s1 - s2;
+
+    if (m1 < m2) {
+        result_m = 10 + m2 - m1;
+        h1--;
+    } else result_m = m1 - m2;
+
+    result_h = h1 - h2;
 }
 
 //===============================================================
@@ -250,10 +334,7 @@ function exerciseTimerFcn() {
                 if (exer_remain > 0) { // do next exercise
                     exer_idx++;
                     prepareNewExercise();
-                    removeHighlight();
-                    scrollToNextExercise();
-                    hightlightExercise();
-
+                    setupExerciseContainer();
                     startExercise();
                 }
                 else { 
@@ -262,6 +343,7 @@ function exerciseTimerFcn() {
                         exer_remain = Sets[set_idx].Exercises.length;
                         exer_idx = 0;
                         prepareNewExercise();
+                        setupExerciseContainer();
                         speakLine('Repeat ' + set_name, startExercise);
                     }
                     else { 
@@ -274,8 +356,8 @@ function exerciseTimerFcn() {
                             exer_remain     = Sets[set_idx].Exercises.length;
                             exer_idx = 0;
                             prepareNewExercise();
-                            loadExerciseContainer();
-                            hightlightExercise();
+ //                           loadExerciseContainer();
+                            setupExerciseContainer();
                             speakLine('Next is ' + set_name, startExercise);
                         }
                         else {
