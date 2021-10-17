20a4a9b8b97ce62a88467246090aa18c94453eac
Wai138 <92008242+Wai138@users.noreply.github.com>
2021-10-17 12:16:16 +1100

    Hmmm

--- timer.js
+++ timer.js
@@ -170,7 +170,7 @@ function loadExerciseContainer () {
 
         let butn = `<table id="exer` + i + `" class="exerciseItem" style="width:100vw;">
         <tr>
-            <td style="text-align:left;font-weight: bold; font-size: 1.5em">` + currExer.exer_name + `</td>
+            <td style="text-align:left;font-weight: bold; font-size: 1.3em;width: 100vw";>` + currExer.exer_name + `</td>
         </tr>
         <tr>
             <td style="text-align:left;width:33.3333%;">Duration: 
