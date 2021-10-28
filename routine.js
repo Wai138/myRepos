b23ff874773d7cc9c972cd58c15aa65b3d0c2954
Wai138 <92008242+Wai138@users.noreply.github.com>
2021-10-28 14:18:24 +1100

    Only start left

--- routine.js
+++ routine.js
@@ -10,33 +10,76 @@ function clockFcn() {
 }
 
 function deleteRout (i) {
-    console.log('i is:' + i)
     document.getElementById("row"+i).remove();
 }
 
 
 function editClick() {
 
-    let r = document.querySelectorAll(".delIcon")
+    let r, s;
+    s = document.querySelector("#routineContainer").scrollTop;
+
+    r = document.querySelectorAll(".spanStartDel");
+    r.forEach(function (elm) {
+        elm.style.position = "fixed";
+    });
+
+    r = document.querySelectorAll(".delIcon")
     r.forEach(function (elm) {
         elm.scrollIntoView({ inline: "start" });
     }); 
-    document.getElementById("divEditDone").innerHTML = '<button id="Done" onclick="doneClick()">Done</button>'
+    document.getElementById("divEditDone").innerHTML = '<button id="Done" onclick="doneClick()">Done</button>';
 
+    r = document.querySelectorAll(".spanStartDel");
+    r.forEach(function (elm) {
+        elm.style.position = "relative";
+
+    });
+    document.querySelector("#routineContainer").scrollTop = s;
 }
-function doneClick() {
 
-    let r = document.querySelectorAll(".routine")
+function doneClick(sDId) {
+
+    let r, s;
+    s = document.querySelector("#routineContainer").scrollTop;
+
+    r = document.querySelectorAll(".spanStartDel");
+    r.forEach(function (elm) {
+        elm.style.position = "fixed";
+    });
+
+    r = document.querySelectorAll(".routine")
     r.forEach(function (elm) {
             elm.scrollIntoView({ inline: "start" });
         }); 
     document.getElementById("divEditDone").innerHTML = '<button id="Edit" onclick="editClick()">Edit</button>'
 
+    r = document.querySelectorAll(".spanStartDel");
+    r.forEach(function (elm) {
+        elm.style.position = "relative";
+    });
+
+    let elm = document.querySelectorAll(".routineButt");
+    elm.forEach(function(s) {
+        let j = s.id.length - sDId.length;
+        let i = s.id.substr(-1 * j);
+        console.log ('i:' + i + ' j:' + j + ' sDId:' + sDId.length + ' s.id:' + s.id.length)
+        s.setAttribute('onclick',  'location.href=\'timer.html?selectedRoutineNo=' + i + '\'');
+        s.style.color = "white";
+        s.style.backgroundColor ="darkblue";
+        s.innerText = 'Start';
+    })
+    document.querySelector("#routineContainer").scrollTop = s;
 }
 
 function shiftDel (i) {
-    let t = document.querySelector("#DelId" + i)
-    t.scrollIntoView({ inline: "start"})
+    let s = document.getElementById('startDelId' + i);
+    s.setAttribute('onclick',  'deleteRout(' + i +');');
+    s.style.color = "yellow";
+    s.style.backgroundColor = "red";
+    s.innerText = 'DELETE';
+//    let t = document.querySelector("#DelId" + i)
+//    t.scrollIntoView({ inline: "start"})
 //    document.getElementById("start" + i).style.display="none";
 }
 
