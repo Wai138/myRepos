73858df8b00f90bac19157f2b2c06bd8d33352eb
Wai138 <92008242+Wai138@users.noreply.github.com>
2021-10-29 14:44:31 +1100

    Stablise start

--- routine.js
+++ routine.js
@@ -14,8 +14,21 @@ function deleteRout (i) {
 }
 
 function editClick(sDIdLit) {
-    let d = document.querySelector('.routineDel');
-    var routineDelWidth = d.offsetWidth;
+    let e, rect, a, b;
+//    var routineDelWidth;
+
+/*    console.log('EditClick')
+    d = document.querySelector('.routineDel');
+    routineDelWidth = d.offsetWidth;
+    rect = d.getBoundingClientRect();
+    console.log('routineDel before:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+*/
+    e = document.querySelector('.routineButt');
+//    routineDelWidth = e.offsetWidth;
+    rect = e.getBoundingClientRect();
+//    console.log('routineButt before:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+
+    b = Math.round(rect.right);
 
     let r, s;
     s = document.querySelector("#routineContainer").scrollTop;
@@ -27,14 +40,51 @@ function editClick(sDIdLit) {
     document.getElementById("divEditDone").innerHTML 
             = '<button id="Done" onclick="doneClick(\'' + sDIdLit + '\')">Done</button>';
 
+
+/*    d = document.querySelector('.routineDel');
+    routineDelWidth = d.offsetWidth;
+    rect = d.getBoundingClientRect();
+    console.log('routineDel after:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+*/
+    e = document.querySelector('.routineButt');
+//    routineDelWidth = e.offsetWidth;
+    rect = e.getBoundingClientRect();
+//    console.log('routineButt after:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+
+    a = Math.round(rect.right);
+
     document.querySelector("#routineContainer").scrollTop = s;
-    $('.routineButt').css('margin-left', '-=' + routineDelWidth + 'px');
+    $('.routineButt').css('margin-left', '-=' + (a - b) + 'px');
+
+/*    d = document.querySelector('.routineDel');
+    routineDelWidth = d.offsetWidth;
+    rect = d.getBoundingClientRect();
+    console.log('routineDel last:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+
+    e = document.querySelector('.routineButt');
+    routineDelWidth = e.offsetWidth;
+    rect = e.getBoundingClientRect();
+    console.log('routineButt last:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+*/                
 }
 
 function doneClick(sDIdLit) {
 
-    let d = document.querySelector('.routineDel');
-    var routineDelWidth = d.offsetWidth;
+    let e, rect, a, b;
+    var routineDelWidth;
+/*
+    console.log('doneClick')
+    d = document.querySelector('.routineDel');
+    routineDelWidth = d.offsetWidth;
+    rect = d.getBoundingClientRect();
+    console.log('routineDel before:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+*/
+    e = document.querySelector('.routineButt');
+//    routineDelWidth = e.offsetWidth;
+    rect = e.getBoundingClientRect();
+//    console.log('routineButt before:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+
+    b = Math.round(rect.right)
 
     let r, s;
     s = document.querySelector("#routineContainer").scrollTop;
@@ -56,9 +106,33 @@ function doneClick(sDIdLit) {
         s.style.backgroundColor ="darkblue";
         s.innerText = 'Start';
     })
+/*
+    d = document.querySelector('.routineDel');
+    routineDelWidth = d.offsetWidth;
+    rect = d.getBoundingClientRect();
+    console.log('routineDel after:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+*/
+    e = document.querySelector('.routineButt');
+//    routineDelWidth = e.offsetWidth;
+    rect = e.getBoundingClientRect();
+//    console.log('routineButt after:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+    
+    a = Math.round(rect.right);
 
-    $('.routineButt').css('margin-left', '+=' + routineDelWidth + 'px');
+    $('.routineButt').css('margin-left', '+=' + (b - a) + 'px');
+
     document.querySelector("#routineContainer").scrollTop = s;
+
+/*    d = document.querySelector('.routineDel');
+    routineDelWidth = d.offsetWidth;
+    rect = d.getBoundingClientRect();
+    console.log('routineDel last:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+
+    e = document.querySelector('.routineButt');
+    routineDelWidth = e.offsetWidth;
+    rect = e.getBoundingClientRect();
+    console.log('routineButt last:', routineDelWidth, Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom), Math.round(rect.left));
+*/                
 }
 
 function dispDelBtn (i, sDIdLit) {
